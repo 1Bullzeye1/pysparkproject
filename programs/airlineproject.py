009ac06bb827346f0266867cfa15cf112bcdb51b
1Bullzeye1 <64720586+1Bullzeye1@users.noreply.github.com>
2022-05-08 12:11:17 +0530

    commiting project utills files and folders

---
+++ programs/airlineproject.py
@@ -0,0 +1,17 @@
+from sparksessionutils import
+
+
+airlineschema = StructType([StructField("airline_id", IntegerType()),
+                            StructField("name", StringType()),
+                            StructField("alias", StringType()),
+                            StructField("iata", StringType()),
+                            StructField("icao", StringType()),
+                            StructField("callsign", StringType()),
+                            StructField("country", StringType()),
+                            StructField("active", StringType())])
+airdf = spark.read.csv(r"D:\airline data\airline.csv",schema=airlineschema,header=True)
+airdf.printSchema()
+airdf.show()
+
+
+
