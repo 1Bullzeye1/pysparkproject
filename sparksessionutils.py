bb365fae1a0568684440966043f9d57e66dfffc2
1Bullzeye1 <64720586+1Bullzeye1@users.noreply.github.com>
2022-05-08 11:55:42 +0530

    commiting project utills files and folders

--- sparksessionutils.py
+++ sparksessionutils.py
@@ -8,14 +8,3 @@ if __name__ == "__main__":
             .config("spark.ui.port","4050").getOrCreate()
     # print(spark)
 
-    airlineschema = StructType([StructField("airline_id", IntegerType()),
-                                StructField("name", StringType()),
-                                StructField("alias", StringType()),
-                                StructField("iata", StringType()),
-                                StructField("icao", StringType()),
-                                StructField("callsign", StringType()),
-                                StructField("country", StringType()),
-                                StructField("active", StringType())])
-    airdf = spark.read.csv(r"D:\pysparkproject\input\airline.csv", schema=airlineschema, header=True)
-    airdf.printSchema()
-    airdf.show()
